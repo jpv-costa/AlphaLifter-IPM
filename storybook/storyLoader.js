ebfc6ee8e93ffe4af4c77c737a7af7d5770d2e8f
Jonas Rodrigues <jg.rodrigues@campus.fct.unl.pt>
2019-11-16 07:54:43 +0000

    Add Form SelectList and ChooseProgressionStory

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,30 +4,30 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('../components/Button/Button.stories');
 	require('../components/Icon/Icon.stories');
 	require('../components/List/List.stories');
 	require('../components/addExercise/ExerciseType.stories');
+	require('../components/button/Button.stories');
 	require('../components/cards/libraryProgramCard/LibraryProgramCard.stories');
 	require('../components/cards/libraryWorkoutCard/LibraryWorkoutCard.stories');
 	require('../components/cards/workoutCard/WorkoutCard.stories');
 	require('../components/charts/LineChart/LineChart.stories');
-	require('../components/chooseProgretion/ChooseProgretion.stories');
+	require('../components/form/Form.stories');
 	require('../components/navigation/TabNavigator.stories');
 	require('../components/workoutTime/WorkoutTime.stories');
 	require('../components/workouts/Exercises/Exercise.stories');
 }
 
 const stories = [
-	'../components/Button/Button.stories',
 	'../components/Icon/Icon.stories',
 	'../components/List/List.stories',
 	'../components/addExercise/ExerciseType.stories',
+	'../components/button/Button.stories',
 	'../components/cards/libraryProgramCard/LibraryProgramCard.stories',
 	'../components/cards/libraryWorkoutCard/LibraryWorkoutCard.stories',
 	'../components/cards/workoutCard/WorkoutCard.stories',
 	'../components/charts/LineChart/LineChart.stories',
-	'../components/chooseProgretion/ChooseProgretion.stories',
+	'../components/form/Form.stories',
 	'../components/navigation/TabNavigator.stories',
 	'../components/workoutTime/WorkoutTime.stories',
 	'../components/workouts/Exercises/Exercise.stories'
