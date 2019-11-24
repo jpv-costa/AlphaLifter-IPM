4c5a0a75b0301ea4f6af0bdea10e9de8b2f921c4
Jonas Rodrigues <jg.rodrigues@campus.fct.unl.pt>
2019-11-24 00:41:23 +0000

    Fix bugs

--- store/reducer.js
+++ store/reducer.js
@@ -7,6 +7,7 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    console.log("payload") 
     console.log(action.payload);
     switch(action.type) {
         case actionTypes.ADD_PROGRAM: { //DONE
@@ -25,7 +26,8 @@ const reducer = (state = initialState, action) => {
                     workout = index+1;
                 }
             })
-            let newPrograms = {...state.programs};
+            let newPrograms = state.programs.filter(() => true);
+            console.log(newPrograms);
             let newProgram = {...state.programs[program-1]};
             newProgram.workouts.concat(workout);
             newPrograms.splice(program-1,1,newProgram);
@@ -49,7 +51,7 @@ const reducer = (state = initialState, action) => {
         }
         case actionTypes.ASSIGN_PROGRESSION_TO_EXERCISE: { //DONE
             let { workout, exercise, progression, targetWeight, targetReps1, targetReps2, targetRir1, targetRir2 } = action.payload;
-            let newWorkouts = {...state.workouts};
+            let newWorkouts = state.workouts.filter(() => true);
             let newWorkout = {...newWorkouts[workout-1]};
             newWorkout.exercises.concat({exercise: exercise, progression:progression, targetWeight: targetWeight, targetReps1:targetReps1, targetReps2:targetReps2, targetRir1:targetRir1, targetRir2:targetRir2});
             newWorkouts.splice(workout-1,1,newWorkout);
@@ -62,7 +64,7 @@ const reducer = (state = initialState, action) => {
         case actionTypes.ASSIGN_EXERCISES_TO_WORKOUT: { //Done
             let { workout, exercises } = action.payload;
 
-            let newWorkouts = {...state.workouts};
+            let newWorkouts = state.workouts.filter(() => true);
             let newWorkout = {...state.workouts[workout-1]};
             newWorkout.exercises.concat(exercises);
             newWorkouts.splice(workout-1,1,newWorkout);
